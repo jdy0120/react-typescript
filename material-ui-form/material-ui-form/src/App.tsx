e4e34dd94cf9ee0b740a6129736d2b292890852a
개발자 <gaebalja@gaebaljaui-MacBookPro.local>
2021-02-23 11:24:32 +0900

    react+typescript projects

---
+++ material-ui-form/material-ui-form/src/App.tsx
@@ -0,0 +1,21 @@
+import { Icon, IconButton, TextField } from '@material-ui/core';
+import React, {useState} from 'react';
+
+import AddIcon from '@material-ui/icons/Add'
+import DeleteIcon from '@material-ui/icons/Delete'
+
+interface TodoItem {
+  id: number;
+  value: string;
+}
+
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ test0/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+import logo from './logo.svg';
+
+const App = () => {
+  const [] = usestate();
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;

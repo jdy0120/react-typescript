6e27868935ab998d73e62cb46c9a354817e41704
개발자 <gaebalja@gaebaljaui-MacBookPro.local>
2021-02-23 11:27:51 +0900

    rm cache

---
+++ reactshoppingcart/react-shopping-cart/src/index.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const client = new QueryClient();
+
+ReactDOM.render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>,document.getElementById('root')
+);
